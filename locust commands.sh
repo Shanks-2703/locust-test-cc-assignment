52a0ce6ed9a62af5ea53708dd3801dcb83b20e78
Shankar <shankar@talently.com>
2025-05-03 21:22:36 +0530

    Update locust commands.sh

--- locust commands.sh
+++ locust commands.sh
@@ -3,27 +3,27 @@ git clone
 
 cd terraform-google-three-tier-web-app
 
-gcloud run services describe tiered-web-app-api --region asia-south1    #replace tiered-web-app-api with the cloud run instance of backend API
+gcloud run services describe three-tier-app-api --region us-central1    #replace tiered-web-app-api with the cloud run instance of backend API
 
-gcloud run services describe tiered-web-app-fe --region asia-south1     #replace tiered-web-app-fe with the cloud run instance of Frontend
+gcloud run services describe three-tier-app-fe --region us-central1     #replace tiered-web-app-fe with the cloud run instance of Frontend
 
-gcloud run services update tiered-web-app-fe --region asia-south1 --set-env-vars "ENDPOINT=https://tiered-web-app-api-880158088034.asia-south1.run.app"   #replace ENDPOINT URL with the cloud run instance of backend URL
+gcloud run services update three-tier-app-fe --region us-central1 --set-env-vars "ENDPOINT=https://three-tier-app-api-137710723753.us-central1.run.app"   #replace ENDPOINT URL with the cloud run instance of backend URL
 
-gcloud sql instances patch three-tier-app-db-8eef \
+gcloud sql instances patch three-tier-app-db-4097 \
   --assign-ip
 
 # grab your Cloud Shell external IP
 export MY_IP=$(curl -s https://ifconfig.me)
-gcloud sql instances patch  tiered-web-app-db-044f \
+gcloud sql instances patch  three-tier-app-db-4097 \
   --authorized-networks="${MY_IP}/32"
 
 pkill cloud_sql_proxy 
 
 ./cloud_sql_proxy \
-  -instances="sonorous-saga-458113-n7:asia-south1:tiered-web-app-db-044f=tcp:5432" \
+  -instances="three-tier-web-app-457409:us-central1:three-tier-app-db-4097=tcp:5432" \
   -ip_address_types=PUBLIC &
 
-gcloud sql users set-password postgres --instance=tiered-web-app-db-044f --password=Newpassword --project=sonorous-saga-458113-n7
+gcloud sql users set-password postgres --instance=three-tier-app-db-4097 --password=Newpassword --project=three-tier-web-app-457409
 
 psql "host=127.0.0.1 port=5432 dbname=todo user=postgres password=Newpassword sslmode=disable"
 
